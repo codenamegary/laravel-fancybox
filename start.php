543ce28b6b6e36352beaf4fb25500c013947c872
Gary Saunders <garysaunders1981@gmail.com>
2012-03-10 10:15:03 -0800

    Commented out some options in start.php

--- start.php
+++ start.php
@@ -11,9 +11,9 @@ Asset::container('fancybox')->add('fancybox','jquery.fancybox-1.3.4.pack.js');
 Asset::container('fancybox')->add('fancybox2','fancybox.js');
 
 // UN-COMMENT for jQuery easing support
-Asset::container('fancybox')->add('fancybox3','jquery.easing-1.3.pack.js');
+// Asset::container('fancybox')->add('fancybox3','jquery.easing-1.3.pack.js');
 
 // UN-COMMENT for mousewheel support in galleries
-Asset::container('fancybox')->add('fancybox4','jquery.mousewheel-3.0.4.pack.js');
+// Asset::container('fancybox')->add('fancybox4','jquery.mousewheel-3.0.4.pack.js');
 
 Asset::container('fancybox')->add('fancybox5','jquery.fancybox-1.3.4.css');
