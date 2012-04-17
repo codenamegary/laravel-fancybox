73c8adc28a5fa829ff75067ec3a3688b1fb0f0d8
codenamegary <garysaunders1981@gmail.com>
2012-04-17 12:15:07 -0300

    Updated start.php - Undesired "js/" in the container line for the jquery library.

--- start.php
+++ start.php
@@ -5,7 +5,7 @@ Autoloader::map(array('FancyBox' => __DIR__.'/fancybox.php'));
 Asset::container('fancybox')->bundle('fancybox');
 
 // UN-COMMENT LINE BELOW IF YOU DO NOT HAVE JQUERY LOADED!
-//Asset::container('fancybox')->add('jquery','js/jquery-1.7.1.min.js');
+//Asset::container('fancybox')->add('jquery','jquery-1.7.1.min.js');
 
 Asset::container('fancybox')->add('fancybox','jquery.fancybox-1.3.4.pack.js');
 Asset::container('fancybox')->add('fancybox2','fancybox.js');
